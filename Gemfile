a6c8bdea1fe35b78f33a991de46896b7c1e6f719
Nick K <nkokkos@gmail.com>
2019-03-21 18:15:00 +0000

    start index page

--- Gemfile
+++ Gemfile
@@ -1,10 +1,9 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.6.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2.2'
+gem 'rails'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
 gem "sqlite3", "~> 1.3.6"
--- config/puma.rb
+++ config/puma.rb
@@ -1,34 +1,25 @@
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-#
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+# This goes in the app as config/puma.rb
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port        ENV.fetch("PORT") { 3000 }
+environment 'development'
+workers 1
+threads 1,4
+preload_app!
+daemonize true
+pidfile 'tmp/pids/puma.pid'
+stdout_redirect 'log/puma.log', 'log/puma.log', true
 
-# Specifies the `environment` that Puma will run in.
-#
-environment ENV.fetch("RAILS_ENV") { "development" }
+bind 'unix://tmp/sockets/puma.sock'
+state_path 'tmp/pids/puma.state'
 
-# Specifies the number of `workers` to boot in clustered mode.
-# Workers are forked webserver processes. If using threads and workers together
-# the concurrency of the application would be max `threads` * `workers`.
-# Workers do not work on JRuby or Windows (both of which do not support
-# processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+on_worker_boot do |worker_index|
 
-# Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load code
-# before forking the application. This takes advantage of Copy On Write
-# process behavior so workers use less memory.
-#
-# preload_app!
+  # write worker pid
+  File.open("tmp/pids/puma_worker_#{worker_index}.pid", "w") { |f| f.puts Process.pid }
 
-# Allow puma to be restarted by `rails restart` command.
-plugin :tmp_restart
+  # reconnect to redis
+  # Redis.current.client.reconnect
+
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
